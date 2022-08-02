4b5d1a5c448549ed4d92bdc050f3d1af1f81992a
Clara <claraswanson671@gmail.com>
2022-08-02 14:14:56 +0200

    Create admin dir

---
+++ admin/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <?php include("../inc/head-info.php");?>
+
+</head>
+
+<body>
+
+    <?php include("../inc/header.php");?>
+
+    <div class="content">
+
+        <?php
+
+            if (isset($_GET['page'])) {
+                switch ($_GET['page']) {
+                    case 'kontakt':
+                        include('pages/kontakt.php');
+                    break;
+                    case 'page1':
+                        include('pages/page1.php');
+                    break;
+                    case 'page2':
+                        include('pages/page2.php');
+                    break;
+                    default:
+                        if ($_GET['page'] == '') {
+                            include($_SERVER['PHP_SELF']);
+                        }
+                        else {
+                            echo '<p>Fehler 404 - Seite nicht gefunden</p>';
+                            header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+                        }
+                    break;
+                    } 
+                    
+            }
+
+        ?>
+
+    </div>
+    
+    <?php include("../inc/footer.php");?>
+
+</body>
+</html>
