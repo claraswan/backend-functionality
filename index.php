f614f5e65cb18f372d8bccd73723b6cfe4fbcd85
Clara <claraswanson671@gmail.com>
2022-08-02 09:39:33 +0200

    Fix index bug

--- index.php
+++ index.php
@@ -16,31 +16,34 @@
         
         // 1.3: GET Parameter mit 'page' als Name
         // und Seitennamen als Wert erstellen
-        $page = $_GET['page'];
 
-        switch ($_GET['page']) {
-            case 'kontakt':
-                include('pages/kontakt.php');
-            break;
-            case 'page1':
-                include('pages/page1.php');
-            break;
-            case 'page2':
-                include('pages/page2.php');
-            break;
-            case 'daten':
-                include('daten.php');
-            break;
-            default:
-                if ($_GET['page'] == '') {
-                    include($_SERVER['PHP_SELF']);
-                }
-                else {
-                    echo '<p>Fehler 404 - Seite nicht gefunden</p>';
-                    header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
-                }
-            break;
-            }
+        if (isset($_GET['page'])) {
+            switch ($_GET['page']) {
+                case 'kontakt':
+                    include('pages/kontakt.php');
+                break;
+                case 'page1':
+                    include('pages/page1.php');
+                break;
+                case 'page2':
+                    include('pages/page2.php');
+                break;
+                case 'daten':
+                    include('daten.php');
+                break;
+                default:
+                    if ($_GET['page'] == '') {
+                        include($_SERVER['PHP_SELF']);
+                    }
+                    else {
+                        echo '<p>Fehler 404 - Seite nicht gefunden</p>';
+                        header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+                    }
+                break;
+                } 
+                
+        }
+        
         
         ?>
     </div>
